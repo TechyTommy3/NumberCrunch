ea6cd1789a1d60696fd4df602edfb26693f2d9f2
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2021-09-05 15:39:47 -0500

    Add an emergency endless loop

--- numbercrunch.py
+++ numbercrunch.py
@@ -1,7 +1,8 @@
 print("NOTE: NumberCrunch is not a random number generator.")
 print("NumberCrunch")
-number = input("Enter in a number to crunch: ")
-print("Crunching...")
-number = int(number)
-number2 = (((number * number) * 2) - number + (number * 2)) * number * (number * number) - (number * number - number * (number * number)) - number
-print(number2)
+while 0 == 0:
+    number = input("Enter in a number to crunch: ")
+    print("Crunching...")
+    number = int(number)
+    number2 = (((number * number) * 2) - number + (number * 2)) * number * (number * number) - (number * number - number * (number * number)) - number
+    print(number2)
