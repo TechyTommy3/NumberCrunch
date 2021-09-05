8494390e4ae4b82820a7e9f4612a1ae94fe910b6
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2021-09-05 15:24:25 -0500

    Add an algahtrim
    We need a nice algtharim.

--- numbercrunch.py
+++ numbercrunch.py
@@ -1,1 +1,7 @@
 print("NOTE: NumberCrunch is not a random number generator.")
+print("NumberCrunch")
+number = input("Enter in a number to crunch: ")
+print("Crunching...")
+number = int(number)
+number2 = ((number * number) * 2) - number + (number * 2)
+print(number2)
