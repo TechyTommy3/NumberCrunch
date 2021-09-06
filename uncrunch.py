14648931e616d043686a4b31e27ceeefa320e605
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2021-09-06 08:06:47 -0500

    Do some more

--- uncrunch.py
+++ uncrunch.py
@@ -1,4 +1,7 @@
 import random
 print("unCrunch")
 number = input("Enter in a NumberCrunched number to uncrunch: ")
-number2 = 
+def rand():
+    return random.randint(1, 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
+number2 = rand()
+number3 = (((number * number) * 2) - number + (number * 2)) * number * (number * number) - (number * number - number * (number * number)) - number
