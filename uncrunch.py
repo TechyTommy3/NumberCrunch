86fdd51f713a32277a87a5712112afe6bf12e543
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2021-09-06 08:14:53 -0500

    Revert "Do some more"
    This reverts commit 14648931e616d043686a4b31e27ceeefa320e605.

--- uncrunch.py
+++ uncrunch.py
@@ -1,7 +1,4 @@
 import random
 print("unCrunch")
 number = input("Enter in a NumberCrunched number to uncrunch: ")
-def rand():
-    return random.randint(1, 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
-number2 = rand()
-number3 = (((number * number) * 2) - number + (number * 2)) * number * (number * number) - (number * number - number * (number * number)) - number
+number2 = 
